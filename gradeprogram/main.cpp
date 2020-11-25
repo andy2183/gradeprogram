ebc8ac69bc6cc88a5e71a1a4a3723d83ac23779d
andy2183 <abtamara21@gmail.com>
2020-11-24 18:24:48 -0800

    Initial Commit

---
+++ gradeprogram/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  gradeprogram
+//
+//  Created by Andy Tamara on 11/24/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
